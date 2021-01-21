9540a2c8b19bc0f3e5a6ec0f6722106cf1f7f5d6
ramirabeau <ramirabeau@gmail.com>
2021-01-20 22:00:07 -0600

    Updated code, added test functions.

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,7 @@
+# config/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
 """config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +22,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("posts.urls")),
 ]
